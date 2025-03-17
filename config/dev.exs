528129230e085acfe7302864607319b9bbabc9ee
Stephen Lewis <gpg@stephenlewis.me>
2025-03-17 21:47:51 +0000

    Store SQLite files in a separate data directory

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :ollie, Ollie.Repo,
-  database: Path.expand("../ollie_dev.db", __DIR__),
+  database: Path.expand("../data/ollie_dev.db", __DIR__),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ollie, Ollie.Repo,
-  database: Path.expand("../ollie_test.db", __DIR__),
+  database: Path.expand("../data/ollie_test.db", __DIR__),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
