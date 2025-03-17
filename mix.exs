63c509ae63cb2632c3b3001f182839abfb65fe7c
Stephen Lewis <gpg@stephenlewis.me>
2025-03-17 23:35:55 +0000

    Run phx.gen.auth

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Ollie.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:bandit, "~> 1.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dns_cluster, "~> 0.1.1"},
